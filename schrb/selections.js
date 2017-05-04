16e46fc020fcbfcfba27e74d582abb6bd7c6c908
Jury Razumau <jurazumau@gmail.com>
2017-05-04 14:10:16 +0300

    add schrb folder

---
+++ schrb/selections.js
@@ -0,0 +1,49 @@
+var users = [];
+
+$.getJSON("https://popping-inferno-4625.firebaseio.com/users-schrb.json", function (list) {
+	for (var key in list) {
+		var user = list[key];
+		if (user.remains < 150 && user.teams) {
+			users.push({name: user.name, teams: user.teams});
+		}
+	}
+	React.render(React.createElement(List, {users: users}), document.getElementById('center'));
+});
+
+
+var List = React.createClass({displayName: "List",
+	render: function() {
+		var users = [];
+		this.props.users.forEach(function (user) {
+			users.push(React.createElement(User, {user: user}));
+		});
+		shuffle(users);
+		return (
+			React.createElement("div", {className: "list"}, 
+			React.createElement("a", {href:"https://fantasy.razumau.net/schrb"}, "← к выбору команд"),
+			users
+			)
+		);
+	}
+	
+});
+
+var User = React.createClass({displayName: "User",
+	render: function() {
+		var teams = [];
+		this.props.user.teams.forEach(function (team) {
+			teams.push(React.createElement("p", null, team.name));
+		});
+		return (
+			React.createElement("div", {className: "userTeams"}, 
+				React.createElement("h3", null, " ", this.props.user.name, " "), 
+				teams
+			)
+		);
+	}
+});
+
+function shuffle(o){
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+}
