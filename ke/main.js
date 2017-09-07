fd17cf6ded8d9b227def4e046b47c80d1b722c4b
Jury Razumau <jurazumau@gmail.com>
2017-09-07 19:36:41 +0300

    update deadline

--- ke/main.js
+++ ke/main.js
@@ -78,7 +78,7 @@ var UserBox = React.createClass({
 				React.createElement("div", {id: "remainingPoints"}, this.state.user.remains),
 				React.createElement("p", null, "Не больше пяти команд."),
 				React.createElement("p", null, "Результат — сумма ответов команд."),
-				React.createElement("p", null, "Изменения можно делать до 7 сентября."),
+				React.createElement("p", null, "Изменения можно делать до 12:00 8 сентября."),
 				React.createElement("div", {id:"selectionsLinkRules"}, 
 					React.createElement("a", {href:"https://fantasy.razumau.net/ke/selections", id: "selectionsLink"}, "Кого выбрали другие?"))
 				)
